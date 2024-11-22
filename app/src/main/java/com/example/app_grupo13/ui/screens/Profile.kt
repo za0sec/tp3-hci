450e1b85d23c00d8b6947005fe02bc2b088da6c0
ozacagnino <ozacagnino@itba.edu.ar>
2024-11-22 12:14:23 -0300

    Profile.kt almost finished

--- app/src/main/java/com/example/app_grupo13/ui/screens/Profile.kt
+++ app/src/main/java/com/example/app_grupo13/ui/screens/Profile.kt
@@ -27,30 +27,40 @@ import com.example.app_grupo13.R
 import com.example.app_grupo13.ui.components.NavBar
 
 @Composable
-fun ProfileScreen(navController: NavController){
+fun ProfileScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF17171F))
             .padding(16.dp)
-    ){
-        Text(
-            text = "Información del usuario",
-            color = Color.White,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Información del usuario",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
         var nombre by remember {
-            mutableStateOf("Octavio") }
+            mutableStateOf("Octavio")
+        }
         var apellido by remember {
-            mutableStateOf("") }
+            mutableStateOf("Zacagnino")
+        }
         var correo by remember {
-            mutableStateOf("") }
+            mutableStateOf("ozacagnino@itba.edu.ar")
+        }
         var alias by remember {
-            mutableStateOf("") }
+            mutableStateOf("ozacagnino")
+        }
         var cvu by remember {
-            mutableStateOf("1234567") }
+            mutableStateOf("123456789")
+        }
 
         Text(
             text = "Nombre",
@@ -58,14 +68,18 @@ fun ProfileScreen(navController: NavController){
             fontSize = 14.sp,
             modifier = Modifier.padding(bottom = 4.dp)
         )
+
+        var isNombreEditable by remember { mutableStateOf(false) } // Estado de edición para Apellido
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             BasicTextField(
                 value = nombre,
                 onValueChange = { nombre = it },
                 modifier = Modifier.weight(1f),
+                readOnly = !isNombreEditable,
                 textStyle = TextStyle(color = Color.White),
                 decorationBox = { innerTextField ->
                     Box(
@@ -79,7 +93,8 @@ fun ProfileScreen(navController: NavController){
                 }
             )
             Spacer(modifier = Modifier.width(8.dp))
-            IconButton(onClick = {},
+            IconButton(
+                onClick = { isNombreEditable = !isNombreEditable },
                 modifier = Modifier.height(38.dp).wrapContentSize()
             ) {
                 Icon(
@@ -90,14 +105,138 @@ fun ProfileScreen(navController: NavController){
             }
         }
 
-        //agregar aca apellido, correo, alias
+        // Espacio entre Nombre y Apellido
+        Spacer(modifier = Modifier.height(16.dp))
 
+
+        // Subtítulo "Apellido"
+        Text(
+            text = "Apellido",
+            color = Color.White,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+
+        // Campo Apellido con icono
+        var isApellidoEditable by remember { mutableStateOf(false) } // Estado de edición para Apellido
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = apellido,
+                onValueChange = { apellido = it },
+                textStyle = TextStyle(color = Color.White),
+                readOnly = !isApellidoEditable, // Controla la edición
+                modifier = Modifier.weight(1f),
+                decorationBox = @Composable { innerTextField ->
+                    Box(
+                        modifier = Modifier
+                            .border(1.dp, Color.White, RoundedCornerShape(4.dp))
+                            .background(Color.Transparent)
+                            .padding(8.dp)
+                    ) {
+                        innerTextField()
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(
+                onClick = {
+                    isApellidoEditable = !isApellidoEditable
+                }, // Cambia el estado de edición
+                modifier = Modifier.height(38.dp).wrapContentSize()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_edit),
+                    contentDescription = "Editar",
+                    tint = Color(0xFF4A347F)
+                )
+            }
+        }
+
+
+        // Espacio entre Apellido y Mail
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        // Subtítulo "Apellido"
+        Text(
+            text = "Correo electrónico",
+            color = Color.White,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+
+        // Campo Apellido con icono
+        var isCorreoEditable by remember { mutableStateOf(false) } // Estado de edición para Apellido
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = correo,
+                onValueChange = { correo = it },
+                textStyle = TextStyle(color = Color.White),
+                readOnly = !isCorreoEditable, // Controla la edición
+                modifier = Modifier.weight(1f),
+                decorationBox = @Composable { innerTextField ->
+                    Box(
+                        modifier = Modifier
+                            .border(1.dp, Color.White, RoundedCornerShape(4.dp))
+                            .background(Color.Transparent)
+                            .padding(8.dp)
+                    ) {
+                        innerTextField()
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(
+                onClick = { isCorreoEditable = !isCorreoEditable }, // Cambia el estado de edición
+                modifier = Modifier.height(38.dp).wrapContentSize()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_edit),
+                    contentDescription = "Editar",
+                    tint = Color(0xFF4A347F)
+                )
+            }
+        }
+
+        // Espacio entre Mail y CVU
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "CVU: $cvu",
+            text = "CVU",
             color = Color.White,
-            modifier = Modifier.padding(top = 16.dp)
+            modifier = Modifier.padding(bottom = 4.dp)
         )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = cvu,
+                onValueChange = { correo = it },
+                textStyle = TextStyle(color = Color.White),
+                readOnly = true, // Controla la edición
+                modifier = Modifier.weight(1f),
+                decorationBox = @Composable { innerTextField ->
+                    Box(
+                        modifier = Modifier
+                            .border(1.dp, Color.White, RoundedCornerShape(4.dp))
+                            .background(Color.Transparent)
+                            .padding(8.dp)
+                    ) {
+                        innerTextField()
+                    }
+                }
+            )
+        }
 
     }
 }
